587aca7e2237d59a9be62cefa202e4e32f920f5d
2354409410 <2354409410@qq.com>
2021-04-21 01:09:42 +0800

    nacos config 测试

---
+++ mall-auth/src/main/java/org/trt/micro/controller/NacosConfigTestController.java
@@ -0,0 +1,21 @@
+package org.trt.micro.controller;
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class NacosConfigTestController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
